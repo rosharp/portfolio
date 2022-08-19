638dbb388e748d720a6f70df4942e8847ac9cff3
rosharp <ro.moreko@gmail.com>
2022-08-19 22:00:51 +0500

    Navbar close on click, fixed padding on header-btn

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,50 +1,50 @@
 import React, { useState } from "react";
-import { NavLink, Outlet } from "react-router-dom";
-import { HashLink as Link } from 'react-router-hash-link';
+import { Outlet } from "react-router-dom";
+import { HashLink as Link } from "react-router-hash-link";
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import { SidebarData } from "./SidebarData";
-import { IoMdTennisball } from "react-icons/io";
+import { IconContext } from "react-icons";
 
 const Layout = () => {
-    const [sidebar, setSidebar] = useState(false);
-    const showSidebar = () => setSidebar(!sidebar);
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
 
-    return (
-        <>
+  return (
+    <>
         <div className="navbar">
-            <Link className="menu-bars" to="#" onClick={showSidebar}>
-                <FaIcons.FaBars className="navbar-show" />
-            </Link>
+          <Link className="menu-bars" to="#" onClick={showSidebar}>
+            <FaIcons.FaBars className="navbar-show" />
+          </Link>
         </div>
         <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-            <ul className="nav-links">
-                <li className="navbar-toggle">
-                    <Link className="menu-bars" to="#" onClick={showSidebar}>
-                        <AiIcons.AiOutlineClose />
-                    </Link>
-                </li>
+          <ul className="nav-links">
+            <li className="navbar-toggle">
+              <Link className="menu-bars" to="#" onClick={showSidebar}>
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
 
-                {SidebarData.map((item, index) => {
-                    return (
-                        <li key={index} className={item.className}>
-                            <Link to={item.path}>
-                                {item.icon}
-                                <span>{item.title}</span>
-                            </Link>
-                        </li>
-                    );
-                })}
-            </ul>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.className}>
+                  <Link to={item.path} onClick={showSidebar}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
         </nav>
 
-        <div className="App">
-            <Outlet />
-        </div>
+      <div className="App">
+        <Outlet />
+      </div>
 
-        <footer>2022</footer>
-        </>
-    );
-}
+      <footer>2022</footer>
+    </>
+  );
+};
 
 export { Layout };
