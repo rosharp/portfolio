6ed153dfc13f0fd578689a63665b5a29bd90f742
Ro Sharp <ro.moreko@gmail.com>
2022-09-02 21:11:04 +0500

    Implement HashRouter for GH Pages deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
-import { Layout } from "./components/Layout";
+// import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import AnimatedRoutes from "./components/AnimatedRoutes";
 
 function App() {
--- src/components/AnimatedRoutes.jsx
+++ src/components/AnimatedRoutes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./Home";
 import { Layout } from "./Layout";
 import NotFound from "./NotFound";
@@ -15,17 +15,17 @@ function AnimatedRoutes() {
     const location = useLocation();
     return (
         <AnimatePresence>
-        <Routes location={location} key={location.pathname}>
-            <Route path="/" element={<Layout />}>
-                <Route index element={<Home />} />
-                <Route path="resume" element={<Resume />} />
-                <Route path="shopit" element={<Shopit />} />
-                <Route path="intro" element={<Intro />} />
-                <Route path="space" element={<Space />} />
-                <Route path="rating" element={<Rating />} />
-                <Route path="*" element={<NotFound />} />
-            </Route>
-        </Routes>
+            <Routes location={location} key={location.pathname}>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="resume" element={<Resume />} />
+                    <Route path="shopit" element={<Shopit />} />
+                    <Route path="intro" element={<Intro />} />
+                    <Route path="space" element={<Space />} />
+                    <Route path="rating" element={<Rating />} />
+                    <Route path="*" element={<NotFound />} />
+                </Route>
+            </Routes>
         </AnimatePresence>
     )
 }
