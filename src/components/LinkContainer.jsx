6010245de1990f772f955adc5ae6f454928eb41f
Ro Sharp <ro.moreko@gmail.com>
2022-09-04 21:00:41 +0500

    Replace CV

--- src/components/LinkContainer.jsx
+++ src/components/LinkContainer.jsx
@@ -7,7 +7,7 @@ const LinkContainer = () => {
             <a className="link-btn" href="https://www.linkedin.com/in/vitaliy-bekshnev-8399b4248/"><FaIcons.FaLinkedin className="link-icon" /></a>
             <a className="link-btn" href="https://github.com/rosharp"><FaIcons.FaGithub className="link-icon" /></a>
             <a className="link-btn" href="mailto:vy.bekshnev@gmail.com"><FaIcons.FaMailBulk className="link-icon" /></a>
-            <a className="link-btn" href="./downloads/Vitaliy_Bekshnev_CV.pdf" download><FaIcons.FaFileDownload className="link-icon" /></a>
+            <a className="link-btn" href="./downloads/Vitaly Bekshnev CV ENG.pdf" download><FaIcons.FaFileDownload className="link-icon" /></a>
         </div>
     )
 }
